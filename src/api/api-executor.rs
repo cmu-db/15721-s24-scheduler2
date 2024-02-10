94b5110f45a2670b54edbd83aefbaeca0f461cbd
Makoto Tomokiyo <makototomokiyo@gmail.com>
2024-02-09 20:01:05 -0500

    compiled api executor

--- src/api/api-executor.rs
+++ src/api/api-executor.rs
@@ -1,4 +1,32 @@
-mod composable_database {
+use tonic::{transport::Server, Request, Response, Status};
+
+use composable_database::{ScheduleQueryArgs, ScheduleQueryRet, QueryInfo};
+
+pub mod composable_database {
     tonic::include_proto!("composable_database");
 }
 
+#[derive(Debug, Default)]
+pub struct SchedulerService {}
+
+#[tonic::async_trait]
+impl Scheduler for SchedulerService {
+    async fn schedule_query(
+        &self,
+        request: Request<ScheduleQueryArgs>,
+    ) -> Result<Response<ScheduleQueryRet>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = hello_world::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    Ok(())
+}
