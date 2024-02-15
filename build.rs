7b85b6eb7ca29e902a9ef5351bb2a3fb0fce3f19
Mingkang <mingkanl@andrew.cmu.edu>
2024-02-14 20:41:47 -0500

    reformat build.rs

--- build.rs
+++ build.rs
@@ -3,13 +3,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = prost_build::Config::new();
     config.out_dir("src");
     config.compile_protos(
-        &["proto/common.proto", "proto/optimizer_scheduler.proto", "proto/scheduler_executor.proto"], 
-        &["proto"])?;
+        &[
+            "proto/common.proto",
+            "proto/optimizer_scheduler.proto",
+            "proto/scheduler_executor.proto",
+        ],
+        &["proto"],
+    )?;
     tonic_build::configure()
         .build_server(true) // Set to false if you don't need server code
         .build_client(false) // Set to false if you don't need client code
         .compile(
-            &["proto/common.proto", "proto/optimizer_scheduler.proto", "proto/scheduler_executor.proto"],
+            &[
+                "proto/common.proto",
+                "proto/optimizer_scheduler.proto",
+                "proto/scheduler_executor.proto",
+            ],
             &["proto"], // Specify the directory where .proto files reside
         )?;
     Ok(())
