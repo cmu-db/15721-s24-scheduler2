624496c497c101b14538ec2599c98d0d54aa7c4a
Mingkang <mingkanl@andrew.cmu.edu>
2024-02-14 21:27:16 -0500

    finish parser and remove substrait submodule

--- build.rs
+++ build.rs
@@ -11,22 +11,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         &["proto"],
     )?;
 
-    // Compile Substrait proto files
-    config.compile_protos(
-        &[
-            "substrait/proto/substrait/algebra.proto",
-            "substrait/proto/substrait/capabilities.proto",
-            "substrait/proto/substrait/extended_expression.proto",
-            "substrait/proto/substrait/function.proto",
-            "substrait/proto/substrait/parameterized_types.proto",
-            "substrait/proto/substrait/plan.proto",
-            "substrait/proto/substrait/type.proto",
-            "substrait/proto/substrait/type_expressions.proto",
-            "substrait/proto/substrait/extensions/extensions.proto",
-        ],
-        &["substrait/proto"], // Include directory for Substrait proto files
-    )?;
-
     tonic_build::configure()
         .build_server(true) // build server side code only
         .build_client(false)
--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,25 @@
+use prost::Message;
+use serde_json;
+use std::io::{self, Cursor};
+use substrait::proto::Plan;
 
+pub enum Format {
+    Json,
+    Binary,
+}
+
+pub fn deserialize_plan(data: &[u8], format: Format) -> Result<Plan, Box<dyn std::error::Error>> {
+    match format {
+        Format::Json => {
+            // Deserialize JSON to Plan
+            let plan = serde_json::from_slice::<Plan>(data)?;
+            Ok(plan)
+        }
+        Format::Binary => {
+            // Deserialize binary to Plan
+            let mut cursor = Cursor::new(data);
+            let plan = Plan::decode(&mut cursor)?;
+            Ok(plan)
+        }
+    }
+}
