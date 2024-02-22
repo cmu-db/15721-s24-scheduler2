97fca6e59e0d5a82e366a45e45678d4b9b3a415e
Mingkang <mingkanl@andrew.cmu.edu>
2024-02-21 21:40:33 -0500

    finish get_execution_plan_from_file

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,85 @@
 use datafusion::physical_plan::ExecutionPlan;
 use datafusion::execution::context::SessionContext;
 use std::{path::PathBuf, time::Duration};
+use std::sync::Arc;
+use async_trait::async_trait;
+use datafusion::arrow::error::ArrowError;
+use datafusion::common::DataFusionError;
+use datafusion::sql::sqlparser::parser::ParserError;
+use sqllogictest::{Record, TestError};
+use thiserror::Error;
+
+
+use sqllogictest::{ColumnType, DBOutput};
+use log::info;
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum DFColumnType {
+    Boolean,
+    DateTime,
+    Integer,
+    Float,
+    Text,
+    Timestamp,
+    Another,
+}
+
+impl ColumnType for DFColumnType {
+    fn from_char(value: char) -> Option<Self> {
+        match value {
+            'B' => Some(Self::Boolean),
+            'D' => Some(Self::DateTime),
+            'I' => Some(Self::Integer),
+            'P' => Some(Self::Timestamp),
+            'R' => Some(Self::Float),
+            'T' => Some(Self::Text),
+            _ => Some(Self::Another),
+        }
+    }
+
+    fn to_char(&self) -> char {
+        match self {
+            Self::Boolean => 'B',
+            Self::DateTime => 'D',
+            Self::Integer => 'I',
+            Self::Timestamp => 'P',
+            Self::Float => 'R',
+            Self::Text => 'T',
+            Self::Another => '?',
+        }
+    }
+}
+
+pub(crate) type DFOutput = DBOutput<DFColumnType>;
+
+pub type Result<T, E = DFSqlLogicTestError> = std::result::Result<T, E>;
+
+/// DataFusion sql-logicaltest error
+#[derive(Debug, Error)]
+pub enum DFSqlLogicTestError {
+    /// Error from sqllogictest-rs
+    #[error("SqlLogicTest error(from sqllogictest-rs crate): {0}")]
+    SqlLogicTest(#[from] TestError),
+    /// Error from datafusion
+    #[error("DataFusion error: {0}")]
+    DataFusion(#[from] DataFusionError),
+    /// Error returned when SQL is syntactically incorrect.
+    #[error("SQL Parser error: {0}")]
+    Sql(#[from] ParserError),
+    /// Error from arrow-rs
+    #[error("Arrow error: {0}")]
+    Arrow(#[from] ArrowError),
+    /// Generic error
+    #[error("Other Error: {0}")]
+    Other(String),
+}
+
+impl From<String> for DFSqlLogicTestError {
+    fn from(value: String) -> Self {
+        DFSqlLogicTestError::Other(value)
+    }
+}
+
 
 
 pub struct IntegrationTestRunner {
@@ -9,8 +88,8 @@ pub struct IntegrationTestRunner {
 }
 
 impl IntegrationTestRunner {
-    pub fn new(ctx: SessionContext, relative_path: PathBuf) -> Self {
-        Self { ctx, relative_path }
+    pub fn new(ctx: SessionContext, relative_path: &str) -> Self {
+        Self { ctx, relative_path: relative_path.into() }
     }
 }
 
@@ -59,36 +138,40 @@ async fn run_query(ctx: &SessionContext, sql: impl Into<String>) -> Result<DFOut
 }
 
 
+async fn get_execution_plan_from_file(file_name: &str) -> Result<Vec<Arc<dyn ExecutionPlan>>, Box<dyn std::error::Error>> {
+    let ctx = SessionContext::new();
 
-// Integration tests: 
-// 1. convert SQL -> ExecutionPlan
-// 2. set up mock executors (gRPC)
-// 3. set up scheduler (gRPC), hardcode mock executors
-// 3. for each executionplan: send to scheduler, collect results and compare
 
-#[cfg(test)]
-mod tests {
-    
-    // Returns a vector of execution plans inside a .slt file
-    async fn get_execution_plan_from_file(file_name: &str) -> Result<Vec<ExecutionPlan>, Box<dyn std::error::Error>> {
-        let ctx = SessionContext::new();
-        let mut tester = sqllogictest::Runner::new(IntegrationTestRunner);
-        let sql_statments = sqllogictest::parse_file(file_name)
-            .expect("Failed to parse file");
-        
-        let mut plans = Vec::new(); 
-        for sql_statement in sql_statements {
-            let plan = ctx.sql(&sql_statement).await?;
-            let plan = plan.create_physical_plan().await?; 
-            plans.push(plan);
-        }
-        Ok(plans)
+    sqllogictest::Runner::new(|| async {
+        Ok(IntegrationTestRunner::new(
+            SessionContext::new(),
+            file_name,
+        ))
+    });
+
+    let sql_statements: Vec<Record<DFColumnType>> = sqllogictest::parse_file(file_name)
+        .expect("Failed to parse file");
+
+    let mut plans = Vec::new();
+    for sql_statement in sql_statements {
+        let plan = ctx.sql(&sql_statement.to_string()).await?;
+        let plan: Arc<dyn ExecutionPlan> = plan.create_physical_plan().await?;
+        plans.push(plan);
     }
-
-
-    #[test]
-    fn test_get_execution_plan() {
-        get_execution_plan_from_file("./test_files/aggregate.slt")
-    }
-
+    Ok(plans)
 }
+
+
+// #[cfg(test)]
+// mod tests {
+//
+//     // Returns a vector of execution plans inside a .slt file
+//
+//
+//
+//     #[test]
+//     fn test_get_execution_plan() {
+//         get_execution_plan_from_file("./test_files/aggregate.slt")
+//     }
+//
+// }
