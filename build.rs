28a3d5aa8d9f294eb648f2606df6461472c3e709
Aidan Smith <adsmith2@andrew.cmu.edu>
2024-04-03 05:33:34 -0400

    Fix generated file not found error

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ fn main() -> Result<(), String> {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
-        .out_dir("src/generated") // Probably should remove, but useful for debugging.
         .compile(&["proto/common.proto"], &["proto"])
         .map_err(|e| format!("Failed to compile protos {:?}", e))
 }
