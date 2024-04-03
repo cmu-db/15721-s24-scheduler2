6c07e31260266c35520e286b40d4fd9964f64594
Aidan Smith <adsmith2@andrew.cmu.edu>
2024-04-03 04:35:25 -0400

    Add python script for downloading TPC-H queries and tables (parquet format)

---
+++ download_tpch_parquet_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import duckdb
+import pyarrow.parquet as pq
+
+scale_factor = 0.01 # scale_factor=1 <=> 1GB database
+
+con = duckdb.connect(database=':memory:')
+con.execute("INSTALL tpch; LOAD tpch")
+con.execute(f"CALL dbgen(sf={scale_factor})")
+
+queries = con.execute("FROM tpch_queries()").fetchall()
+tables = con.execute("show tables").fetchall()
+
+# for (i, q) in queries:
+#     with open(f"{i}.sql", "w") as file:
+#         file.writelines(q)
+# for (t,) in tables:
+#     res = con.query("SELECT * FROM " + t)
+#     pq.write_table(res.to_arrow_table(), t + ".parquet")
